c1cbe3a13fbbcbab0762072834bf21b082334c92
1TheGreatest <soloeben12@gmail.com>
2025-03-02 19:02:05 +0000

    clerk auth added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +16,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "SavvyCent - Every cent counts, and your AI knows it.",
-  description: "SavvyCent is a personal finance app that helps  you save money.",
+  description:
+    "SavvyCent is a personal finance app that helps  you save money.",
 };
 
 export default function RootLayout({
@@ -23,12 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body 
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Header />
+          <main className="min-h-screen">{children}</main>
+
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto text-center text-gray-600">
+              <p className=""> Made with love by TheGreatest </p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
